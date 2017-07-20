aea32ed3eb2869dc1cd76fdcefbe2f397f3f9b1b
lnlindao <lnlindao@hotmail.com>
2017-07-20 00:05:57 -0500

    alertDialog

---
+++ Dialogs/AlertDialog/app/src/main/java/com/example/diana/myapplication/MainActivity.java
@@ -0,0 +1,55 @@
+﻿package com.example.diana.myapplication;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnDate, btnTime;
+    EditText txtDate, txtTime;
+    private int año, mes, dia, hora, minute;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final CharSequence listaElementos[] = {"Guayaquil", "Quito", "Cuenca"};
+        final ArrayList<Integer> mSelectedItems = new ArrayList();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Lista Ciudades");
+        builder.setMultiChoiceItems(listaElementos, null,
+                new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which,
+                                        boolean isChecked) {
+                        if (isChecked) {
+                            mSelectedItems.add(which);
+                            Toast.makeText(getApplicationContext(),
+                                    "Item Seleccionado  " + mSelectedItems.size(), Toast.LENGTH_LONG).show();
+                        } else if (mSelectedItems.contains(which)) {
+                            mSelectedItems.remove(Integer.valueOf(which));
+                        }
+                    }
+                });
+        builder.create();
+        builder.show();
+
+    }
+}
+
